4c5922d4de301b5c01d949e092b3fd75060def9a
Sam Ruchti <samuelpruchti@gmail.com>
2018-04-09 13:45:01 -0400

    Object Game

--- textgame.py
+++ textgame.py
@@ -15,22 +15,48 @@ class Hostile_Monster(Creatures):
     """"""
 
 
-
-print("You wake up in a daze, you don't recognize your surroundings."
-      "What do you decide to do?")
-
-x = input("1) Explore and get familiar with your surroundings.\n"
-          "2) Stay where you are and see what happens.\n"
+def adv():
+    print("You wake up in a daze, you don't recognize your surroundings.\n"
+          "What do you decide to do?")
+    print("")
+    x = input("1) Explore and get familiar with your surroundings.\n"
+              "2) Stay where you are and see what happens.\n"
               "3) Stay in the area and wait for civilization to come to you.\n")
-if x.find("1") >= 0:
-    print("You decided to explore, but you encountered a Hostile Enemy and"
-          "died")
-    h = Hostile_Monster('Enemy')
-    h.health(100)
+    print("")
+    if x.find("1") >= 0:
+        print("You decided to explore, but you encountered a Hostile Enemy and"
+              "died")
+        h = Hostile_Monster('Enemy')
+        h.health(100)
 
-if x.find("2") >= 0:
-    print("You are safe...for now")
-    print 
-if x.find("3") >= 0:
-    print("It has been a substantial amount of time...no one has come"
-          "you'll die if you dont do something soon.")
+    if x.find("2") >= 0:
+        print("You are safe...for now")
+        print("")
+        def stay_put():
+            print("It is now getting dark. What is your next move?")
+            x = input("1) Wait until nightfall comes so you can move in darkness and be "
+                  "concealed\n"
+                  "2) Find a place to spend the night\n"
+                  "3) Find a source of food and water\n"
+                  "4) Find materials for weapons\n")
+        
+            if x.find("1") >= 0:
+                print("Smart of you to wait for Nightfall, but it didnt help you"
+                      " too much.\n"
+                      "You died because you were defenseless.")
+            if x.find("2") >= 0:
+                print("Something found you in the night while you were sleeping/n"
+                      "You were Killed")
+            if x.find("3") >= 0:
+                print("You now have food and water to last you tonight and"
+                      "tomorrow")
+            if x.find("4") >= 0:
+                print("You found some colored fishing line in your pocket and a "
+                      "bendable stick, so you make a bow\n"
+                      "there's just one problem\n"
+                      "...Arrows")
+        stay_put()
+    if x.find("3") >= 0:
+        print("It has been a substantial amount of time...no one has come,\n"
+              "you'll die if you don't do something soon.")
+adv()
