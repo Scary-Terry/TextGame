70b100526d511a348ff3694017f93853a0454cb9
Sam Ruchti <samuelpruchti@gmail.com>
2018-04-17 13:51:35 -0400

    restart mechanic added

--- textgame.py
+++ textgame.py
@@ -1,6 +1,9 @@
-#Sam Ruchti
-#Text Adventure
-#4/3/18
+"""
+    Sam Ruchti
+    Text Adventure
+    4/3/18
+"""
+import time
 
 class Creatures:
 
@@ -12,46 +15,159 @@ class Creatures:
 
 
 class Hostile_Monster(Creatures):
-    """"""
+     """"""
 
-print ("To restart the game, type adv() and hit enter at anytime.")
+print ('To restart the game, type "restart" and hit enter at anytime.\n'
+       "To quit the game type quit and hit enter.\n")
+
+    
 def adv():
-    print("You wake up in a daze, you don't recognize your surroundings.\n"
-          "What do you decide to do?")
+
+    
+    s = ("You wake up in a daze, you don't recognize your surroundings.\n"
+         "What do you decide to do?")
+    for letter in s:
+        print(letter, end='')
+        time.sleep(.1)
+    print('')
+
     print("")
     x = input("1) Explore and get familiar with your surroundings.\n"
               "2) Stay where you are and see what happens.\n"
               "3) Stay in the area and wait for civilization to come to you.\n")
-    print("")
-    if x.find("1") >= 0:
-        print("You decided to explore, but you encountered a Hostile Enemy and "
-              "died")
-        h = Hostile_Monster("Enemy")
-        print (h)
-        
+    if x.find("restart") >= 0:
+        s = "You have chosen the easy way out"
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.1)
+        print('')
+        s = "You will be revived when the progress bar is complete"
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.1)
+        print('')
+        s = "[=======================================================]"
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.5)
+        print('')
+        adv()
 
+    if x.find("quit") >= 0:
+        s = "You have chosen the easy way out"
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.1)
+        print('')
+        s = "You will be revived when the progress bar is complete"
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.1)
+        print('')
+        s = "[=======================================================]"
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.5)
+        print('')
+
+    print("")
+    
+    if x.find("1") >= 0:
+        s = "You decided to explore, but you encountered a Hostile Enemy while unarmed."
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.1)
+        print('')
+        s = "You Have Died"
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.1)
+        print('')
+        s = "You will be revived when the progress bar is complete"
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.1)
+        print('')
+        s = "[=======================================================]"
+        for letter in s:
+            print(letter, end='')
+            time.sleep(.5)
+        print('')
+        adv()
+    
     if x.find("2") >= 0:
         print("You are safe...for now")
         print("")
         def stay_put():
             print("It is now getting dark. What is your next move?")
             x = input("1) Wait until nightfall comes so you can move in darkness and be "
-                  "concealed\n"
-                  "2) Find a place to spend the night\n"
-                  "3) Find a source of food and water\n"
-                  "4) Find materials for weapons\n")
-        
+                      "concealed\n"
+                      "2) Find a place to spend the night\n"
+                      "3) Find a source of food and water\n"
+                      "4) Find materials for weapons\n")
+            
             if x.find("1") >= 0:
-                print("Smart of you to wait for Nightfall, but it didnt help you"
-                      " too much.\n"
-                      "You died because you were defenseless.")
+                s = ("Smart of you to wait for Nightfall, but it didnt help you"
+                     " too much.\n")
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.1)
+                print('')
+                s = "You were killed because you were defenseless"
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.1)
+                print('')
+                s = "You will be revived when the progress bar is complete"
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.1)
+                print('')
+                s = "[=======================================================]"
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.5)
+                print('')
+                adv()
+                
             if x.find("2") >= 0:
-                print("Something found you in the night while you were sleeping/n"
-                      "You were Killed")
+                s = "Something found you in the night while you were sleeping/n You were Killed"
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.1)
+                print('')
+                s = "You will be revived when the progress bar is complete"
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.1)
+                print('')
+                s = "[=======================================================]"
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.5)
+                print('')
+                adv()
+                
             if x.find("3") >= 0:
-                print("You now have food and water to last you tonight and "
+                s = ("You now have food and water to last you tonight and "
                       "tomorrow\n"
-                      "but you did not make it to the next day...You died in your sleep")
+                      "but you did not make it to the next day...You died in your sleep "
+                      "mysteriously")
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.1)
+                print('')
+                s = "You will be revived when the progress bar is complete"
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.1)
+                print('')
+                s = "[=======================================================]"
+                for letter in s:
+                    print(letter, end='')
+                    time.sleep(.5)
+                print('')
+                adv()
             if x.find("4") >= 0:
                 print("You found some colored fishing line in your pocket and a "
                       "bendable stick, so you make a bow\n"
@@ -66,16 +182,48 @@ def adv():
                               "3) Search to see if you could steal some from people\n")
                     
                     if x.find('1') >= 0:
-                        print ("You couldn't find any sticks fit for arrows...you were defenseless"
-                               "to an attack and you died")
+                        s = ("You couldn't find any sticks fit for arrows...you were defenseless"
+                             "to an attack and you died")
+                        for letter in s:
+                            print(letter, end='')
+                            time.sleep(.1)
+                        print('')
+                        s = "You will be revived when the progress bar is complete"
+                        for letter in s:
+                            print(letter, end='')
+                            time.sleep(.1)
+                        print('')
+                        s = "[=======================================================]"
+                        for letter in s:
+                            print(letter, end='')
+                            time.sleep(.5)
+                        print('')
+                        adv()
                     if x.find('2') >= 0:
                         arrows = 3
                         print ("You have found 3 arrows but they won't last long")
                         def fight():
-                            x = input('')
+                            x = input('Now that you have arrows, What will you do now?\n'
+                                      '1) Get familiar with your surroundings'
+                                      '2) Stay put and defend yourself'
+                                      '3) Go hunting')
                     if x.find('3') >= 0:
                         print ("You went searching and didn't encouter anyone...soon after you return to your temperary home, you are ambushed "
                                "and cannot defend yourself")
+                        for letter in s:
+                            print(letter, end='')
+                            time.sleep(.1)
+                        print('')
+                        s = "You will be revived when the progress bar is complete"
+                        for letter in s:
+                            print(letter, end='')
+                            time.sleep(.1)
+                        print('')
+                        s = "[=======================================================]"
+                        for letter in s:
+                            print(letter, end='')
+                            time.sleep(.5)
+                        print('')
                 arm()
         stay_put()
     if x.find("3") >= 0:
@@ -103,3 +251,37 @@ def adv():
 
 
 adv()
+if x.find("restart") >= 0:
+    s = "You have chosen the easy way out"
+    for letter in s:
+        print(letter, end='')
+        time.sleep(.1)
+    print('')
+    s = "You will be revived when the progress bar is complete"
+    for letter in s:
+        print(letter, end='')
+        time.sleep(.1)
+    print('')
+    s = "[=======================================================]"
+    for letter in s:
+        print(letter, end='')
+        time.sleep(.5)
+    print('')
+
+
+if x.find("quit") >= 0:
+    s = "You have chosen the easy way out"
+    for letter in s:
+        print(letter, end='')
+        time.sleep(.1)
+    print('')
+    s = "You will be revived when the progress bar is complete"
+    for letter in s:
+        print(letter, end='')
+        time.sleep(.1)
+    print('')
+    s = "[=======================================================]"
+    for letter in s:
+        print(letter, end='')
+        time.sleep(.5)
+    print('')
